eb9a4b0ec4dcb79fa60f3350c2a532aca98aa578
Jason Webb <bigjasonwebb@gmail.com>
2010-11-14 00:02:07 -0700

    Work on the field types.

---
+++ pycontract/exceptions.py
@@ -0,0 +1,8 @@
+
+class UnableToCleanError(ValueError):
+    
+    def __init__(self, field=None, value=None, *args, **kwargs):
+        super(UnableToCleanError, self).__init__(*args, **kwargs)
+        
+        self.field = field
+        self.value = value
